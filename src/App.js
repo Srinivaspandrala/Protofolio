2094462054f80db4c7c3e09aee6653eab06c480f
Srinivaspandrala <140938945+Srinivaspandrala@users.noreply.github.com>
2024-09-22 14:00:00 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css'
 import Navbar from './components/Navbar';
-import Preloader from './components/PreLoader';
+import Preloader from './components/Preloader';
 
 function App() {
     return (
